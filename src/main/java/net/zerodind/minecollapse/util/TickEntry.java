b6eda1fb5b8a4acd7e3f14c08e796bcc8b9eef3c
Supheria <supheria@gmail.com>
2026-04-08 14:26:28 +0800

    添加原版代码和资源

---
+++ src/main/java/net/zerodind/minecollapse/util/TickEntry.java
@@ -0,0 +1,40 @@
+package net.zerodind.minecollapse.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+
+public class TickEntry
+{
+    private final BlockPos pos;
+    private int ticks;
+
+    public TickEntry(CompoundTag nbt)
+    {
+        this(BlockPos.of(nbt.getLong("pos")), nbt.getInt("ticks"));
+    }
+
+    public TickEntry(BlockPos pos, int ticks)
+    {
+        this.pos = pos;
+        this.ticks = ticks;
+    }
+
+    public BlockPos getPos()
+    {
+        return pos;
+    }
+
+    public boolean tick()
+    {
+        this.ticks--;
+        return this.ticks == 0;
+    }
+
+    public CompoundTag serializeNBT()
+    {
+        CompoundTag nbt = new CompoundTag();
+        nbt.putLong("pos", pos.asLong());
+        nbt.putInt("ticks", ticks);
+        return nbt;
+    }
+}
